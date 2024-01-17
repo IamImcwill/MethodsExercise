a4799998ece6e7e4011caf64b4fae86819d843f1
Caris Williams <cwill1013@gmail.com>
2024-01-17 17:09:08 -0500

    Exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -3,19 +3,52 @@
     public class Program
     {
         static void Main(string[] args)
+        
+           
+ 
         {
-            Console.WriteLine("What is your name?");
-            string userName = Console.ReadLine();
-            Console.WriteLine("How old are you?");
-            string age = Console.ReadLine();
-            Console.WriteLine("What is the name of your pet if any?");
-            string userPetName = Console.ReadLine();
-            Console.WriteLine("What is the color of your pet?");
-            string userPetColor = Console.ReadLine();
+            
+            console.writeline("what is your name?");
+            string username = console.readline();
+            console.writeline("how old are you?");
+            string age = console.readline();
+            console.writeline("what is the name of your pet if any?");
+            string userpetname = console.readline();
+            console.writeline("what is the color of your pet?");
+            string userpetcolor = console.readline();
 
-            Console.WriteLine($"I met a  young man yesterday whom seemed cool, his name was {userName}. It shocked me when the man told me he was {age} years old.");
-            Console.WriteLine($"He had this beautiful {userPetColor}, Englisg Bulldog named {userPetName}.");
-            Console.WriteLine($"{userName} mentioned to me, that he was looking to breed {userPetName} with a lighter verison of {userPetColor}."); 
+            console.writeline($"i met a  young man yesterday whom seemed cool, his name was {username}. it shocked me when the man told me he was {age} years old.");
+            console.writeline($"he had this beautiful {userpetcolor}, englisg bulldog named {userpetname}.");
+            console.writeline($"{username} mentioned to me, that he was looking to breed {userpetname} with a lighter verison of {userpetcolor}.");
+
+            Console.WriteLine("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+            
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is:{sum}");
+
+            Console.WriteLine("Give me a number");
+             num1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Give me a number to multiply to the first one");
+             num2 = int.Parse(Console.ReadLine());
+
+            int product = Multiply(num1, num2);
+            Console.WriteLine($"The sum is:{product}");
         }
+        
+        public static int Sum(int num1, int num2)
+
+        { return num1 + num2; }
+
+        public static int Multiply(int x, int y)
+
+        { return x * y;}
     }
+
 }
