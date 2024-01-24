c63c5f723b1683d328cb44eb6b46eb82fe97890a
Caris Williams <cwill1013@gmail.com>
2024-01-24 09:21:50 -0500

    Completed

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -7,19 +7,19 @@
            
  
         {
-            
-            //console.writeline("what is your name?");
-            //string username = console.readline();
-            //console.writeline("how old are you?");
-            //string age = console.readline();
-            //console.writeline("what is the name of your pet if any?");
-            //string userpetname = console.readline();
-            //console.writeline("what is the color of your pet?");
-            //string userpetcolor = console.readline();
 
-            //console.writeline($"i met a  young man yesterday whom seemed cool, his name was {username}. it shocked me when the man told me he was {age} years old.");
-            //console.writeline($"he had this beautiful {userpetcolor}, englisg bulldog named {userpetname}.");
-            //console.writeline($"{username} mentioned to me, that he was looking to breed {userpetname} with a lighter verison of {userpetcolor}.");
+            Console.WriteLine("what is your name?");
+            string username = Console.ReadLine();
+            Console.WriteLine("how old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine("what is the name of your pet if any?");
+            string userpetname = Console.ReadLine();
+            Console.WriteLine("what is the color of your pet?");
+            string userpetcolor = Console.ReadLine();
+
+            Console.WriteLine($"I met a  young man yesterday whom seemed cool, his name was {username}. it shocked me when the man told me he was {age} years old.");
+            Console.WriteLine($"He had this beautiful {userpetcolor}, englisg bulldog named {userpetname}.");
+            Console.WriteLine($"{username} mentioned to me, that he was looking to breed {userpetname} with a lighter verison of {userpetcolor}.");
 
             Console.WriteLine("Give me a number to add");
             int num1 = int.Parse(Console.ReadLine());
@@ -27,28 +27,28 @@
 
             Console.WriteLine("Give me a number to add to the first one");
             int num2 = int.Parse(Console.ReadLine());
-            
+
             int sum = Sum(num1, num2);
             Console.WriteLine($"The sum is:{sum}");
 
             Console.WriteLine("Give me a number");
-             num1 = int.Parse(Console.ReadLine());
+            num1 = int.Parse(Console.ReadLine());
 
 
             Console.WriteLine("Give me a number to multiply to the first one");
-             num2 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
 
             int product = Multiply(num1, num2);
             Console.WriteLine($"The sum is:{product}");
         }
-        
+
         public static int Sum(int num1, int num2)
 
         { return num1 + num2; }
 
         public static int Multiply(int x, int y)
 
-        { return x * y;}
+        { return x * y; }
     }
 
 }
--- MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4799998ece6e7e4011caf64b4fae86819d843f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799334584a45739072f6ae9ff3585fd109730ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
